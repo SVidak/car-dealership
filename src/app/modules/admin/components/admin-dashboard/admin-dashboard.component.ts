4968fb9abfa221ea155044422ad0359f25763d65
SVidak <svidakofficial@gmail.com>
2024-06-23 23:25:09 +0200

    admin dashboard

--- src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Car } from '../../../../model/car.model';
+import { CarService } from '../../../../crud/car.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrl: './admin-dashboard.component.scss'
+  styleUrl: './admin-dashboard.component.scss',
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
+  public cars: Car[] = [];
+  public mostExpensiveCar: Car;
+  public averagePrice: number;
+  public totalPrice: number;
 
+  constructor(private service: CarService, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.service.getCars().subscribe((cars) => {
+      this.cars = cars;
+      this.calculateCarStatistics();
+    });
+  }
+
+  private calculateCarStatistics(): void {
+    if (this.cars.length > 0) {
+      this.mostExpensiveCar = this.cars.reduce((prev, curr) =>
+        prev.price > curr.price ? prev : curr
+      );
+      this.totalPrice = this.cars.reduce((sum, car) => sum + car.price, 0);
+      this.averagePrice = this.totalPrice / this.cars.length;
+    }
+  }
 }
