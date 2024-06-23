67fcd704a50d293d4b3b2b0b27c835cb9412b834
SVidak <svidakofficial@gmail.com>
2024-06-23 23:07:42 +0200

    implementing route guards

--- src/app/modules/customer/customer-routing.module.ts
+++ src/app/modules/customer/customer-routing.module.ts
@@ -5,13 +5,14 @@ import { AddComponent } from './components/add/add.component';
 import { CarsComponent } from './components/cars/cars.component';
 import { MyComponent } from './components/my/my.component';
 import { CarDetailsComponent } from './components/car-details/car-details.component';
+import { CustomerGuard } from '../../auth/services/guard/customer-guard.guard';
 
 const routes: Routes = [
-  {path:'dashboard', component: CustomerDashboardComponent},
-  {path:'add', component: AddComponent},
-  {path:'cars', component: CarsComponent},
-  {path:'my', component: MyComponent},
-  {path:'car/:id', component: CarDetailsComponent}
+  { path: 'dashboard', component: CustomerDashboardComponent, canActivate: [CustomerGuard] },
+  { path: 'add', component: AddComponent, canActivate: [CustomerGuard] },
+  { path: 'cars', component: CarsComponent, canActivate: [CustomerGuard] },
+  { path: 'my', component: MyComponent, canActivate: [CustomerGuard] },
+  { path: 'car/:id', component: CarDetailsComponent, canActivate: [CustomerGuard] }
 ];
 
 @NgModule({
